db62c4e5febecaf4666d5eb6f5e595095cb7a50b
Kevin Heise <kheise91@hotmail.com>
2020-06-03 22:48:11 -0500

    Turning 'Intro' into a React component, and setting up the fade in text for initial site load.

--- web-app/src/components/intro.js
+++ web-app/src/components/intro.js
@@ -1,12 +1,40 @@
 import React from 'react';
 import '../css/intro.css';
 
+/*
 function Intro() {
   return (
-    <div id="intro-main">
-
+    <div id="introMain">
+      <div id="bgImg"></div>
+      <h1 id="titleOne" className="title-text">Somnia</h1>
+      <h3 id="titleTwo" className="title-text">Dream Journal</h3>
     </div>
   );
+}
+*/
+
+class Intro extends React.Component {
+  componentDidMount() {
+    this.fadeOne = setTimeout(() => { document.getElementById('titleOne').className += ' fade-in'; }, 1000);
+    this.fadeTwo = setTimeout(() => { document.getElementById('titleTwo').className += ' fade-in'; }, 2000);
+    this.fadeThree = setTimeout(() => { document.getElementById('bgImg').className += ' fade-in'; }, 3500);
+  }
+
+  componentWillUnmount() {
+    clearTimeout(this.fadeOne);
+    clearTimeout(this.fadeTwo);
+    clearTimeout(this.fadeThree);
+  }
+
+  render() {
+    return (
+      <div id="introMain">
+        <div id="bgImg"></div>
+        <h1 id="titleOne" className="title-text">Somnia</h1>
+        <h3 id="titleTwo" className="title-text">Dream Journal</h3>
+      </div>
+    );
+  }
 }
 
 export default Intro;
--- web-app/src/index.js
+++ web-app/src/index.js
@@ -1,9 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './css/reset.min.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/bootstrap-override.scss';
-import './css/index.css';
+import './css/reset.min.css';
 import Intro from './components/intro';
 
 ReactDOM.render(
